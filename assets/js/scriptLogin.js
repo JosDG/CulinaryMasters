86d7efc78ddfceeb9939b728a4525035d9b1e5ae
JosDG <118546489+JosDG@users.noreply.github.com>
2023-11-21 11:00:33 -0500

    Add files via upload

---
+++ assets/js/scriptLogin.js
@@ -0,0 +1,20 @@
+const btnSignIn = document.getElementById("sign-in"),
+  btnSignup = document.getElementById("sign-up"),
+  btnlogin = document.getElementById("Login"),
+  formRegister = document.querySelector(".register"),
+  formLogin = document.querySelector(".login");
+
+btnSignIn.addEventListener("click", (e) => {
+  formRegister.classList.add("hide");
+  formLogin.classList.remove("hide");
+});
+
+btnSignup.addEventListener("click", (e) => {
+  formLogin.classList.add("hide");
+  formRegister.classList.remove("hide");
+});
+
+btnlogin.addEventListener("click", (e) => {
+    window.location.replace("../../indexLogin.html");
+    alert("si");
+});
